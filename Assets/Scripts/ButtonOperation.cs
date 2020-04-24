299129b711242e865605ac2c9dbbbf08d5a899eb
zeffy1014 <kin-s@st.cat-v.ne.jp>
2020-04-25 02:34:31 +0900

    [update]ショットボタン改善
    ショット用ボタンを押しっぱなし対応、押され表現対応　あと家の画像少し変更追加

--- Assets/Scripts/ButtonOperation.cs
+++ Assets/Scripts/ButtonOperation.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+// ボタンの種類
+public enum ButtonKind
+{
+    None,
+    Action_Shot,  // 弾を発射
+    Action_Throw, // 武器を投げる
+
+    System_Retry, // リトライする
+    System_Title, // タイトルへ戻る
+};
+
 
 public class ButtonOperation : MonoBehaviour
 {
     public PlayerController player;
 
+    public ButtonKind buttonKind;
+
+    public Sprite popImage;
+    public Sprite pushImage;
+
     public bool isOperatePanel = false; // プレイ中の操作ボタンかどうか
+
+    private bool isDown = false; // 押された状態かどうか
 
     private void Start()
     {
@@ -14,7 +34,28 @@ public class ButtonOperation : MonoBehaviour
         if (isOperatePanel) TouchController.SetOperateArea(transform);
     }
 
-    /* ボタン押下に対する各種処理群 */
+    private void Update()
+    {
+        // 押されているかどうか
+        if (isDown)
+        {
+            // 押され画像があればそれにする
+            if (pushImage) this.GetComponent<Image>().sprite = pushImage;
+            if (ButtonKind.Action_Shot == buttonKind)
+            {
+                // 弾を撃つ
+                if (player) player.ShotBullet();
+            }
+        }
+        else
+        {
+            // 離され画像があればそれにする
+            if (popImage) this.GetComponent<Image>().sprite = popImage;
+        }
+
+    }
+
+    /* ボタン押下に対する単発の各種処理群 */
     // タイトルに戻る
     public void ReturnTitle()
     {
@@ -27,9 +68,30 @@ public class ButtonOperation : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
     }
 
-    // 弾を撃つ
-    public void ShotBullet()
+    /* 各種ボタン操作に対し処理を行いたい場合 */
+    // ボタン押し
+    public void OnButtonDown()
     {
-        player.ShotBullet();
+        isDown = true;
+
+        if (ButtonKind.Action_Shot == buttonKind)
+        {
+            // 弾を撃つ
+            if (player) player.ShotBullet();
+        }
     }
+
+    // ボタン離し
+    public void OnButtonUp()
+    {
+        isDown = false;
+
+        if (ButtonKind.Action_Shot == buttonKind)
+        {
+            // ボタンを離され表示にする
+            if (popImage) this.GetComponent<Image>().sprite = popImage;
+        }
+
+    }
+
 }
