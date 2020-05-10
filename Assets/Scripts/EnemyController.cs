1e638995c22e1266b81c054a59c95bb59e78326b
zeffy1014 <kin-s@st.cat-v.ne.jp>
2020-05-11 03:32:08 +0900

    [update]敵着弾時エフェクト
    敵に弾が当たってまだ撃破しない際は一瞬光るようにした(当たっていることがわかる)

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -16,19 +16,29 @@ public enum EnemyType
 
 public class EnemyController : MonoBehaviour
 {
-    float fallSpeed;
-    int hp;
-
     public EnemyType enemyType;
+
+    // 移動速度
     public float fallSpeedBase;
+    float fallSpeed;
+
+    // HP
     public int hpBase;
+    int hp;
 
     //public Sprite enemySprite;
     //SpriteRenderer mainSpriteRenderer;
 
-    // 撃破時のエフェクトと音
+    // ダメージ時のエフェクトと音(共通設定)
+    static AudioClip damageSE;         // ダメージの音
+    public Material damageMaterial;    // ダメージ中にスプライトに適用するマテリアル(白くするため)
+    public Material normalMaterial;    // 通常時のマテリアル(もとに戻すため)
+    static float flashTime = 0.1f; // ダメージ表示する時間
+
+    // 撃破時のエフェクトと音(一応個別設定)
     public GameObject defeatEffect;
     public AudioClip defeatSE;
+
     static GameObject tempObject = null;
     static AudioSource audioSource = null;
 
@@ -36,27 +46,20 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         // 各種初期化
-        // とりあえず背景スクロールスピードを加算する(固定的が背景と一緒にスクロールする)
+        // とりあえず背景スクロールスピードを加算する(移動ゼロの敵は背景と一緒にスクロールする)
         fallSpeed = fallSpeedBase + BGController.scrollSpeed;
-        // もともと速度があるものはランダムで上乗せ
+        // もともと速度があるものはランダムで上乗せ(1.0-1.5倍)
         fallSpeed += (fallSpeedBase - BGController.scrollSpeed) * Random.Range(0.0f, 0.5f);
 
         hp = hpBase;
 
-        // 表示画像をランダムで選択(TODO:いずれは生成側で指定)
-        /*
-        mainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        Sprite sprite = null;
-        int index = Random.Range(0, enemySprites.Length);
-        sprite = enemySprites[index];
-        mainSpriteRenderer.sprite = sprite;
-        */
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0, -fallSpeed * Time.deltaTime, 0, Space.World);
+
     }
 
     // ダメージ
@@ -64,15 +67,17 @@ public class EnemyController : MonoBehaviour
     {
         hp -= bulletAtk;
 
+        // AudioSource読み込み
+        if (null == tempObject)
+        {
+            tempObject = new GameObject("EnemyController_Temp");
+            GameObject.DontDestroyOnLoad(tempObject);
+            audioSource = tempObject.AddComponent<AudioSource>();
+        }
+
         if (0 >= hp)
         {
             // 撃破
-            if (null == tempObject)
-            {
-                tempObject = new GameObject("EnemyController_Temp");
-                GameObject.DontDestroyOnLoad(tempObject);
-                audioSource = tempObject.AddComponent<AudioSource>();
-            }
             audioSource.PlayOneShot(defeatSE);
             Instantiate<GameObject>(defeatEffect, transform.position, Quaternion.identity);
 
@@ -82,8 +87,21 @@ public class EnemyController : MonoBehaviour
 
             Destroy(this.gameObject);
         }
+        else
+        {
+            // ダメージ
+            //audioSource.PlayOneShot(damageSE);
+            StartCoroutine(Flash(flashTime));
+        }
 
     }
 
+    // ちょっと光る
+    IEnumerator Flash(float time)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().material = damageMaterial;
+        yield return new WaitForSeconds(time);
+        this.gameObject.GetComponent<SpriteRenderer>().material = normalMaterial;
+    }
 
 }
